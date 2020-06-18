c5cf87a988e8ac52fd0e695483519c7ba4824b0d
Jishunyi <jishunyi131@163.com>
2020-06-18 09:25:12 +0800

    HC32F072_APP

---
+++ HC32F072_APP/USER/can_pro.c
@@ -0,0 +1,122 @@
+/******************************************************************************/
+/**\file can_pro.c
+ * @Author       : Shunyi
+ * @Date         : 2020-06-11 08:33:55
+ * @LastEditors  : Shunyi
+ * @LastEditTime : 2020-06-16 08:51:37
+ ******************************************************************************/
+#include "gpio.h"
+#include "can_pro.h"
+
+/******************************************************************************
+ * variable definitions                                      									*
+ ******************************************************************************/
+stc_can_rxframe_t       stcRxFrame;
+stc_can_txframe_t       stcTxFrame;
+uint8_t                 u8RxFlag = FALSE;
+
+/*******************************************************************************
+ * Function implementation - global ('extern') and local ('static')
+ ******************************************************************************/
+ //CAN中断函数
+void Can_IRQHandler(void)
+{
+    if(TRUE == CAN_IrqFlgGet(CanRxIrqFlg))
+    {
+        CAN_IrqFlgClr(CanRxIrqFlg);
+        CAN_IrqCmd(CanRxIrqEn, FALSE);
+
+        CAN_Receive(&stcRxFrame);
+
+        u8RxFlag = TRUE;
+    }
+}
+
+//< 系统时钟初始化(8MHz for CanClk)
+void App_SysClkInit(void)
+{
+    ///< 切换时钟前（根据外部高速晶振）设置XTH频率范围,配置晶振参数，使能目标时钟，此处为8MHz
+    Sysctrl_SetXTHFreq(SysctrlXthFreq6_12MHz);
+    Sysctrl_XTHDriverCfg(SysctrlXtalDriver3);
+    Sysctrl_SetXTHStableTime(SysctrlXthStableCycle16384);
+    Sysctrl_ClkSourceEnable(SysctrlClkXTH, TRUE);
+    delay1ms(10);
+
+    ///< 时钟切换
+    Sysctrl_SysClkSwitch(SysctrlClkXTH);
+}
+
+//< CAN GPIO 配置
+void App_CanGpioInit(void)
+{
+    stc_gpio_cfg_t stcGpioCfg;
+
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralGpio, TRUE);
+
+    ///< 端口方向配置->输入
+    stcGpioCfg.enDir = GpioDirIn;
+    ///< 端口驱动能力配置->高驱动能力
+    stcGpioCfg.enDrv = GpioDrvL;
+    ///< 端口上下拉配置->无
+    stcGpioCfg.enPu = GpioPuDisable;
+    stcGpioCfg.enPd = GpioPdDisable;
+    ///< 端口开漏输出配置->开漏输出关闭
+    stcGpioCfg.enOD = GpioOdDisable;
+    ///< 端口输入/输出值寄存器总线控制模式配置->AHB
+    stcGpioCfg.enCtrlMode = GpioAHB;
+
+    Gpio_Init(EVB_CAN_RX_PORT, EVB_CAN_RX_PIN, &stcGpioCfg);
+    stcGpioCfg.enDir = GpioDirOut;
+    Gpio_Init(EVB_CAN_TX_PORT, EVB_CAN_TX_PIN, &stcGpioCfg);
+    Gpio_Init(EVB_CAN_STB_PORT, EVB_CAN_STB_PIN, &stcGpioCfg);
+
+    ///<CAN RX\TX复用功能配置
+    Gpio_SetAfMode(EVB_CAN_RX_PORT, EVB_CAN_RX_PIN, GpioAf1);
+    Gpio_SetAfMode(EVB_CAN_TX_PORT, EVB_CAN_TX_PIN, GpioAf1);
+
+    ///<STB 低-PHY有效
+    Gpio_ClrIO(EVB_CAN_STB_PORT, EVB_CAN_STB_PIN);
+}
+
+//< CAN 初始化配置
+void App_CanInit(void)
+{
+    stc_can_init_config_t   stcCanInitCfg;
+    stc_can_filter_t        stcFilter;
+
+
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralCan, TRUE);
+
+    //<<CAN 波特率配置
+    stcCanInitCfg.stcCanBt.PRESC = 1-1;
+    stcCanInitCfg.stcCanBt.SEG_1 = 5-2;
+    stcCanInitCfg.stcCanBt.SEG_2 = 3-1;
+    stcCanInitCfg.stcCanBt.SJW   = 3-1;
+
+    stcCanInitCfg.stcWarningLimit.CanErrorWarningLimitVal = 10;
+    stcCanInitCfg.stcWarningLimit.CanWarningLimitVal = 16-1;
+
+    stcCanInitCfg.enCanRxBufAll  = CanRxNormal;
+    stcCanInitCfg.enCanRxBufMode = CanRxBufNotStored;
+    stcCanInitCfg.enCanSTBMode   = CanSTBFifoMode;
+
+    CAN_Init(&stcCanInitCfg);
+
+    //<<CAN 滤波器配置
+    stcFilter.enAcfFormat = CanAllFrames;
+    stcFilter.enFilterSel = CanFilterSel1;
+    stcFilter.u32CODE     = 0x00000352;
+    stcFilter.u32MASK     = 0x1FFFFFFF;
+    CAN_FilterConfig(&stcFilter, TRUE);
+
+
+    //<<Can Irq Enable
+    CAN_IrqCmd(CanRxIrqEn, TRUE);
+
+    EnableNvic(CAN_IRQn, IrqLevel0, TRUE);
+
+}
+
+/******************************************************************************
+ * EOF (not truncated)
+ ******************************************************************************/
---
+++ HC32F072_APP/USER/uart_pro.c
@@ -0,0 +1,149 @@
+/******************************************************************************/
+/**\file uart_pro.c
+ * @Author       : Shunyi
+ * @Date         : 2020-06-11 08:33:55
+ * @LastEditors  : Shunyi
+ * @LastEditTime : 2020-06-16 08:51:37
+ ******************************************************************************/
+
+/******************************************************************************
+ * Include files
+ ******************************************************************************/
+#include "gpio.h"
+#include "uart.h"
+#include "uart_pro.h"
+#include "user.h"
+#include "stdlib.h"
+
+/******************************************************************************
+ * Local variable definitions ('static')                                      *
+ ******************************************************************************/
+uint8_t Uart1TxCnt = 0;												//发送中断计数
+uint8_t Uart1RxCnt = 0;												//接收中断计数
+uint8_t Uart1TxMax;														//发送数据个数
+uint8_t Uart1RxMax = DataFromMPULen;					//接收数据个数
+uint8_t Uart1TxData[UartSendMax];							//待发送数据
+uint8_t Uart1RxData[UartReceiveMax];					//待接收数据
+
+extern uint8_t DataFromMPUFlag; 		//显示屏回传指令的标志位 //1为接收到数据
+
+/*******************************************************************************
+ * Function implementation - global ('extern') and local ('static')
+ ******************************************************************************/
+//UART1中断函数
+void Uart1_IRQHandler(void)
+{
+    if(Uart_GetStatus(M0P_UART1, UartRC))         								//UART1数据接收
+    { 
+				Uart_ClrStatus(M0P_UART1, UartRC);        								//清中断状态位			
+				Uart1RxData[Uart1RxCnt] = Uart_ReceiveData(M0P_UART1);	
+			
+				Uart1RxCnt++;
+				DataFromMPUFlag = 0; //接收未完成
+				
+				if(Uart1RxCnt >= Uart1RxMax)															//接收结束
+				{
+						Uart1RxCnt = 0;
+						DataFromMPUFlag = 1; //接收完成
+						Uart_DisableIrq(M0P_UART1, UartRxIrq);            		//禁止UART1接收功能
+            Uart_EnableIrq(M0P_UART1, UartTxIrq);              		//使能UART1发送功能
+				}
+    }
+    
+    if(Uart_GetStatus(M0P_UART1, UartTC))         								//UART1数据发送
+    {
+				Uart_ClrStatus(M0P_UART1, UartTC);                        //清除中断状态标志位
+				Uart1TxCnt++;
+				if(Uart1TxCnt >= Uart1TxMax)                            	//发送结束
+        {
+						Uart1TxCnt = 0; 
+            Uart_DisableIrq(M0P_UART1,UartTxIrq);            			//禁止UART1发送中断
+            Uart_EnableIrq(M0P_UART1,UartRxIrq);              		//使能UART1接收中断
+        }
+				else                                       
+        {
+            Uart_SendDataIt(M0P_UART1, Uart1TxData[Uart1TxCnt]);  //继续发送数据字节 
+        }
+    }
+}
+
+//UART1引脚配置
+void App_Uart1PortInit(void)
+{
+    stc_gpio_cfg_t stcGpioCfg;
+    
+    DDL_ZERO_STRUCT(stcGpioCfg);
+    
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralGpio,TRUE); //使能GPIO模块时钟
+    
+    ///<TX
+    stcGpioCfg.enDir = GpioDirOut;
+    Gpio_Init(GpioPortA, GpioPin2, &stcGpioCfg);
+    Gpio_SetAfMode(GpioPortA, GpioPin2, GpioAf1);          //配置PA02 端口为URART1_TX
+    
+    ///<RX
+    stcGpioCfg.enDir = GpioDirIn;
+    Gpio_Init(GpioPortA, GpioPin3, &stcGpioCfg);
+    Gpio_SetAfMode(GpioPortA, GpioPin3, GpioAf1);          //配置PA03 端口为URART1_RX
+}
+
+//UART1参数配置
+void App_Uart1Cfg(void)
+{
+    stc_uart_cfg_t  stcCfg;
+    stc_uart_multimode_t stcMulti;
+    stc_uart_baud_t stcBaud;
+
+    DDL_ZERO_STRUCT(stcCfg);
+    DDL_ZERO_STRUCT(stcMulti);
+    DDL_ZERO_STRUCT(stcBaud);
+
+    //开启外设时钟
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralUart1, TRUE); //使能uart1模块时钟
+
+    //UART1 Init
+    stcCfg.enRunMode = UartMskMode1;                	//模式1 //模式2无数据 //模式3多字节数据有误
+    stcCfg.enStopBit = UartMsk1bit;                 	//1bit停止位
+    stcCfg.enMmdorCk = UartMskEven;                 	//偶检验
+    stcCfg.stcBaud.u32Baud = 9600;                  	//波特率9600
+    stcCfg.stcBaud.enClkDiv = UartMsk8Or16Div;      	//通道采样分频配置
+    stcCfg.stcBaud.u32Pclk = Sysctrl_GetPClkFreq(); 	//获得外设时钟（PCLK）频率值
+    Uart_Init(M0P_UART1, &stcCfg);                  	//串口初始化
+	
+    //UART1中断使能
+    Uart_ClrStatus(M0P_UART1, UartRC);         				//清接收请求
+		Uart_ClrStatus(M0P_UART1, UartTC);         				//清发送请求
+    Uart_EnableIrq(M0P_UART1, UartRxIrq);      				//使能串口接收中断
+		//Uart_EnableIrq(M0P_UART1, UartTxIrq);      				//使能串口发送中断
+    EnableNvic(UART1_3_IRQn, IrqLevel3, TRUE); 				//系统中断使能
+}
+
+//UART1初始化
+void App_Uart1Init(void)
+{
+		//UART1引脚配置
+		App_Uart1PortInit();
+		//UART1参数配置
+		App_Uart1Cfg();
+}
+
+void App_UartSend(M0P_UART_TypeDef* UARTx, const uint8_t *DataSendBuffer, const uint8_t DataSendBufferLen) //串口发送数据
+{
+		//使能串口发送中断
+		Uart_EnableIrq(UARTx, UartTxIrq);   
+	
+		if(UARTx == M0P_UART1) //UART1
+		{
+				//装载待发送数据
+				Uart1TxCnt = 0;
+				Uart1TxMax = DataSendBufferLen;
+				memcpy((void *)Uart1TxData, DataSendBuffer, Uart1TxMax);
+			
+				//启动数据发送
+				Uart_SendDataIt(UARTx, Uart1TxData[0]);			//发送第一个字节 后续数据在中断中发送
+		}
+}
+
+/******************************************************************************
+ * EOF (not truncated)
+ ******************************************************************************/
